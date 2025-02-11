3041b2ff59f3cd86ec62c62685f6e002c5d69212
Andrey Nikolov <avnikolov@gmail.com>
2025-02-11 21:28:57 +0200

    66 plus one arr presentation

--- src/tasks/66-Plus-One/index.ts
+++ src/tasks/66-Plus-One/index.ts
@@ -1,8 +1,27 @@
+// function plusOne(digits: number[]): number[] {
+//     const number = BigInt(digits.join(''))
+//     const result = number + BigInt(1)
+//     return result.toString().split('').map(el => parseInt(el))
+// };
+
 function plusOne(digits: number[]): number[] {
-  const number = BigInt(digits.join(""));
-  const result = number + BigInt(1);
-  return result
-    .toString()
-    .split("")
-    .map((el) => parseInt(el));
+  let shouldCarry = false;
+  for (let i = digits.length - 1; i >= 0; i--) {
+    const iElement = digits[i];
+
+    if (iElement != 9) {
+      digits[i] = iElement + 1;
+      shouldCarry = false;
+      break;
+    } else {
+      digits[i] = 0;
+      shouldCarry = true;
+    }
+  }
+
+  if (shouldCarry) {
+    return [1, ...digits];
+  } else {
+    return digits;
+  }
 }
