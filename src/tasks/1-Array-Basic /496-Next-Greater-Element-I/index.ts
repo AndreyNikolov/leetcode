e162c0f4e2066ea5492f91490778f796a811ab80
Andrey Nikolov <avnikolov@gmail.com>
2025-02-22 17:01:51 +0200

    Add better solution to 496-Next-greter-element

--- src/tasks/1-Array-Basic /496-Next-Greater-Element-I/index.ts
+++ src/tasks/1-Array-Basic /496-Next-Greater-Element-I/index.ts
@@ -1,27 +1,57 @@
-function findNextGreatElement(el: number, nums2: number[]) {
-  let foundIndex = -1;
-  let greaterElement = -1;
+/* Naive
+function findNextGreatElement(el, nums2) {
+    let foundIndex = -1;
+    let greaterElement = -1;
 
-  for (let j = 0; j < nums2.length; j++) {
-    const el2 = nums2[j];
-    if (el === el2) {
-      foundIndex = j;
-      continue;
-    }
-    if (foundIndex !== -1 && el2 > el) {
-      greaterElement = el2;
-      break;
-    }
-  }
-
-  return greaterElement;
+    for (let j=0; j<nums2.length; j++){
+        const el2 = nums2[j]
+        if (el === el2) {
+            foundIndex = j;
+            continue;
+        }
+        if (foundIndex !== -1 && el2 > el) {
+            greaterElement = el2;
+            break;
+        }
+    } 
+    return greaterElement;
 }
 
 function nextGreaterElement(nums1: number[], nums2: number[]): number[] {
-  const answer = [];
+    const answer = []
 
-  for (let i = 0; i < nums1.length; i++) {
-    answer.push(findNextGreatElement(nums1[i], nums2));
+    for (let i=0; i< nums1.length; i++) {
+        answer.push(findNextGreatElement(nums1[i], nums2))
+    }
+    return answer;
+};
+*/
+
+function findNextGreaterElement(element: number, arr: number[]) {
+  for (let i = 0; i < arr.length; i++) {
+    if (element < arr[i]) {
+      return arr[i];
+    }
   }
-  return answer;
+  return -1;
+}
+
+function nextGreaterElement(nums1: number[], nums2: number[]): number[] {
+  const result = new Array(nums1.length).fill(-1);
+
+  // conver the num1 array to a hash map, where the integer is the key, and the index of it is the value
+  const num1Map = new Map();
+  for (let i = 0; i < nums1.length; i++) {
+    num1Map.set(nums1[i], i);
+  }
+
+  for (let j = 0; j < nums2.length; j++) {
+    if (!num1Map.has(nums2[j])) continue;
+
+    const positionOfJInNums1 = num1Map.get(nums2[j]); // 1
+    const greater = findNextGreaterElement(nums2[j], nums2.slice(j + 1));
+    result[positionOfJInNums1] = greater;
+  }
+
+  return result;
 }
