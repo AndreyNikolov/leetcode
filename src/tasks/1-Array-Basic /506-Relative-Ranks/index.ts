f64ebdf1a356f1cbfa0d12628d5b429a96316419
Andrey Nikolov <avnikolov@gmail.com>
2025-02-23 16:11:20 +0200

    Add better solution to 506

--- src/tasks/1-Array-Basic /506-Relative-Ranks/index.ts
+++ src/tasks/1-Array-Basic /506-Relative-Ranks/index.ts
@@ -1,4 +1,4 @@
-function findIndexOfMaxElement(score: number[]) {
+/*function findIndexOfMaxElement(score: number[]) {
   let maxElement = -1;
   let maxElementIndex = -1;
   for (let i = 0; i < score.length; i++) {
@@ -36,4 +36,41 @@ function findRelativeRanks(score: number[]): string[] {
   }
 
   return results;
+}
+  */
+
+function findRelativeRanks(score: number[]): string[] {
+  const result = new Array(score.length).fill("");
+
+  // construct a hash map with the score and its index
+  const scoreMap = new Map();
+  for (let i = 0; i < score.length; i++) {
+    scoreMap.set(score[i], i);
+  }
+
+  // construct rank stack
+  const rankStack = new Array(score.length)
+    .fill("")
+    .map((el, index) => {
+      if (index === 0) {
+        return "Gold Medal";
+      } else if (index === 1) {
+        return "Silver Medal";
+      } else if (index === 2) {
+        return "Bronze Medal";
+      } else {
+        return `${index + 1}`;
+      }
+    })
+    .reverse();
+
+  // sort the scores by highest first
+  const sortedScore = score.sort((a, b) => b - a);
+  for (let i = 0; i < sortedScore.length; i++) {
+    const curr = sortedScore[i];
+    const currOldIndex = scoreMap.get(curr);
+    result[currOldIndex] = rankStack.pop();
+  }
+
+  return result;
 }
